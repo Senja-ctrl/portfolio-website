6cde03c2bef03f806ba98d9e7f59a1a5e67057d0
Senja-ctrl <simpson.julian@outlook.com>
2026-04-08 08:59:19 +0200

    subpage test

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,22 @@
 function SetWindowWidth() {
     document.getElementById("i01").setAttribute("width", window.innerWidth);
     const element = document.getElementById("i02")
-    const x = 55;
-    const y1 = 40;
-    const y2 = y1 + 4;
-    const y3 = y2 + 1;
-    const y4 = y3 + 4;
-    const w = window.innerWidth - 40;
-    const w2 = w - 5;
-    const array = [[x, y1], [w2, y1], [w, y1 - 15], [w, y2], [x + 10, y3], [x + 10, y3], [w2 - 5, y3], [w - 5, y3 - 15], [w - 5, y4], [x, y4], [x, y1]]
+    let x = 55;
+    let y1 = 40;
+    let y2 = y1 + 4;
+    let y3 = y2 + 1;
+    let y4 = y3 + 4;
+    let w = window.innerWidth - 40;
+    let w2 = w - 5;
+    let array = [[x, y1], [w2, y1], [w, y1 - 15], [w, y2], [x + 10, y3], [x + 10, y3], [w2 - 5, y3], [w - 5, y3 - 15], [w - 5, y4], [x, y4], [x, y1]]
     element.setAttribute("points", array);
+}
+
+function OpenPortfolio() {
+    document.getElementsByClassName("PortfolioDiv");
+    open("./portfolio.html", "_self");
+}
+function OpenItch() {
+    open("https://boolianjulian.itch.io", "_self");
 }
 
